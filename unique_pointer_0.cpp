4262e859ac3ae3068f2ba92cc1a14f54ecce87a4
satlawa <p.satlawa@gmx.net>
2020-11-16 20:38:02 +0100

    blue print for a unique smart pointer

---
+++ unique_pointer_0.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+class MyClass
+{
+private:
+    std::string _text;
+
+public:
+    MyClass() {}
+    MyClass(std::string text) { _text = text; }
+    ~MyClass() { std::cout << _text << " destroyed" << std::endl; }
+    void setText(std::string text) { _text = text; }
+    std::string getText() { return _text; }
+};
+
+int main()
+{
+    // create unique pointer to proprietary class
+    std::unique_ptr<MyClass> myClass1(new MyClass());
+    std::unique_ptr<MyClass> myClass2(new MyClass("String 2"));
+
+    // call member function using ->
+    myClass1->setText("String 1");
+
+    std::cout << "myClass1 " << myClass1->getText() << std::endl;
+
+    // use the dereference operator *
+    *myClass1 = *myClass2;
+
+    // use the .get() function to retrieve a raw pointer to the object
+    std::cout << "Objects have stack addresses " << myClass1.get() << " and " << myClass2.get() << std::endl;
+
+    return 0;
+}
